367c608292956326017baa425fe329c33021f314
Yogen Patel <yogenspatel@gmail.com>
2019-04-24 21:14:57 -0400

    Cleaning up code

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import './style.scss';
 
 const Footer = () => (
   <footer>
-    <p>&copy; {new Date().getFullYear()} Even Financial, Inc. - CONFIDENTIAL</p>
+    <p>&copy; {new Date().getFullYear()} Yogen Patel</p>
   </footer>
 );
 
--- src/components/app.js
+++ src/components/app.js
@@ -11,7 +11,6 @@ import history from "../utitilies/history";
 class App extends Component {
   submit = values => {
     this.props.performSearch(values);
-    console.log(values);
     const url = `?searchText=${values.searchText}${values.stars ? `&stars=${values.stars}` : ""}${
       values.hasForked ? "&fork=true" : ""
     }&license=${values.license ? values.license : "mit"}`;
@@ -24,7 +23,7 @@ class App extends Component {
     return (
       <div>
         <Header />
-        <h1 className="heading container display-4">Even Financial GitHub Repository Search</h1>
+        <h1 className="heading container display-4">GitHub Repository Search</h1>
         <GitForm onSubmit={this.submit} />
         {this.props.searchResults && <SearchResults results={this.props.searchResults} />}
         <Footer />
